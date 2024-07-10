74a8d5630f6255e43934ed68f983c78f6f4d2d9a
HemantKumar01 <ytbhemant@gmail.com>
2024-07-10 20:06:17 +0530

    hero and header base

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,13 +2,8 @@ import { images } from "@/constants";
 
 const Header = () => {
   return (
-    <div className="w-full bg-transparent px-6 py-2">
-      <img
-        src={images.logo}
-        alt="Celesta"
-        height={40}
-        className="h-[40px] w-auto"
-      />
+    <div className="absolute top-0 left-0 w-full bg-transparent px-6 py-2 h-[60px]">
+      <img src={images.logo} alt="Celesta" className="h-[40px] w-auto" />
     </div>
   );
 };
--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -1,10 +1,32 @@
 import Header from "@/components/header/header";
+import { images } from "@/constants";
+
+const HeroSection = () => {
+  return (
+    <div
+      className="w-full h-screen overflow-hidden"
+      style={{
+        background: `url(${images.hero_image})`,
+        backgroundSize: "cover",
+        backgroundPosition: "50% 30%",
+      }}
+    >
+      <div
+        className="w-full h-full"
+        style={{
+          background:
+            "linear-gradient(180deg, rgba(0, 0, 0, 0.75) 0%, rgba(0, 0, 0, 0.00) 49.8%, rgba(23, 6, 28, 0.00) 90%, #2F0C39 100%), rgba(0, 0, 0, 0.25)",
+        }}
+      ></div>
+    </div>
+  );
+};
 
 const LandingPage = () => {
   return (
     <>
       <Header></Header>
-      This will be the landing page
+      <HeroSection></HeroSection>
     </>
   );
 };
