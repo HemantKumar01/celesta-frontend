dbd418558e15669a800e32473c4fe05ac5bc3c0d
yashraj <yashrajsinghthakur23@gmail.com>
2024-09-14 18:32:38 +0530

    Modified Navbar.jsx and Navbar.css for mobile-responsive navigation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./navbar.css";
+import "./Navbar.css";
 import DropdownMenu from "./DropdownMenu"; // Importing DropdownMenu
 
 const Navbar = () => {
