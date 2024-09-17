df1463d3f9223307d34c675c7db74cad9386bcc8
Genusuppal <harpranav2202@gmail.com>
2024-09-18 00:43:58 +0530

    Images Optimized

--- src/components/team/team.jsx
+++ src/components/team/team.jsx
@@ -7,8 +7,8 @@ export default function Team_template({ team_name, coordsarray }) {
 					return (
 						<div key={index} className="flex flex-col gap-3 items-center m-1 mb-8 lg:m-5 lg:mb-10">
 							<div
-								className="overflow-hidden lg:h-[200px] lg:w-[190px] w-[120px] h-[120px] shadow-custom hover:shadow-hover p-[3px] rounded-lg bg-black flex justify-center items-center"
-								style={{ backgroundImage: `url(${e.image})`, backgroundSize: "cover" }}></div>
+								className="overflow-hidden lg:h-[250px] lg:w-[200px] w-[120px] h-[150px] shadow-custom hover:shadow-hover p-[3px] rounded-lg bg-black flex justify-center items-center"
+								style={{ backgroundImage: `url(${e.image})`, backgroundSize: "cover", backgroundPosition: "center" }}></div>
 							<div className="overflow-hidden lg:h-[50px] lg:w-[200px] min-w-[120px] text-nowrap min-h-[40px] shadow-custom hover:shadow-hover p-[3px] rounded-lg bg-black flex justify-center items-center">
 								<p>{e.name}</p>
 							</div>
