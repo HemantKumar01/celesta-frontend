3e865162c58b1c5e5d2c50b0ea3cbf040924e75e
Genusuppal <harpranav2202@gmail.com>
2024-09-17 20:52:33 +0530

    Final Navbar Changes

--- src/components/Navbar/DropDown.jsx
+++ src/components/Navbar/DropDown.jsx
@@ -1,67 +1,55 @@
 import { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "./header.css";
+import "./Navbar.css";
 
 function Dropdown() {
-  const [isOpen, setIsOpen] = useState(false);
-  const menuRef = useRef(null);
+	const [isOpen, setIsOpen] = useState(false);
+	const menuRef = useRef(null);
 
-  const handleMouseEnter = () => {
-    setIsOpen(true);
-  };
+	const handleMouseEnter = () => {
+		setIsOpen(current => !current);
+	};
 
-  const handleMouseLeave = () => {
-    setIsOpen(false);
-  };
+	const handleMouseLeave = () => {
+		setIsOpen(false);
+	};
 
-  const handleClickOutside = (event) => {
-    if (menuRef.current && !menuRef.current.contains(event.target)) {
-      setIsOpen(false);
-    }
-  };
+	const handleClickOutside = (event) => {
+		if (menuRef.current && !menuRef.current.contains(event.target)) {
+			setIsOpen(false);
+		}
+	};
 
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
+	useEffect(() => {
+		document.addEventListener("mousedown", handleClickOutside);
+		return () => {
+			document.removeEventListener("mousedown", handleClickOutside);
+		};
+	}, []);
 
-  return (
-    <div className="dropdown">
-      <Link
-        to="/"
-        className="dropdown-toggle"
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      >
-        The Fest
-      </Link>
-      {isOpen && (
-        <div
-          className="dropdown-menu"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-          ref={menuRef}
-        >
-          {/* Your dropdown menu items here */}
-
-          <div>
-            <Link to="/sponsors">Sponsors</Link>
-          </div>
-          {/* <div>
-            <Link to="/">Gallery</Link>
-          </div> */}
-          {/* <div>
-            <Link to="/">Our Team</Link>
-          </div> */}
-          <div>
-            <Link to="/contact">Contacts</Link>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div className="dropdown" onClick={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+			<Link to="/" className="dropdown-toggle">
+				The Fest
+			</Link>
+			{isOpen && (
+				<div className="dropdown-menu" ref={menuRef}>
+					<div>
+						<Link to="/sponsors">Sponsors</Link>
+					</div>
+					<div>
+						<Link to="/gallery">Gallery</Link>
+					</div>
+					<div>
+						<Link to="/team">Our Team</Link>
+					</div>
+					<div>
+						<Link to="/contact">Contacts</Link>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default Dropdown;
